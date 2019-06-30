cf64cff9788b32aff7d751626bb964be7da3b9d6
George Schizas <gschizas@gmail.com>
2019-06-30 08:15:53 +0300

    Cleanup code formatting via PEP8

--- flashcart-builder.py
+++ flashcart-builder.py
@@ -2,129 +2,135 @@ print("\nArduboy Flashcart image builder v1.04 by Mr.Blinky Jun 2018 - May 2019\
 
 # requires PILlow. Use 'python -m pip install pillow' to install
 
+import csv
+import os
 import sys
 import time
-import os
-import csv
-import math
-try:
-  from PIL import Image
-except:
-  print("The PILlow module is required but not installed!")
-  print("Use 'python -m pip install pillow' from the commandline to install.")
-  sys.exit()
 
-ID_LIST  = 0
+try:
+    from PIL import Image
+except:
+    print("The PILlow module is required but not installed!")
+    print("Use 'python -m pip install pillow' from the commandline to install.")
+    sys.exit()
+
+ID_LIST = 0
 ID_TITLE = 1
 ID_TITLESCREEN = 2
 ID_HEXFILE = 3
 ID_DATAFILE = 4
 ID_SAVEFILE = 5
 
+
 def DelayedExit():
     time.sleep(3)
     sys.exit()
 
+
 def DefaultHeader():
     return bytearray("ARDUBOY".encode() + (b'\xFF' * 249))
 
+
 def LoadTitleScreenData(filename):
     if not os.path.isabs(filename):
-      filename = path + filename
-    if not os.path.isfile(filename) :
+        filename = path + filename
+    if not os.path.isfile(filename):
         print("Error: Title screen '{}' not found.".format(filename))
         DelayedExit()
     img = Image.open(filename).convert("1")
-    width, height  = img.size
-    if (width != 128) or (height != 64) :
+    width, height = img.size
+    if (width != 128) or (height != 64):
         print("Error: Title screen '{}' is not 128 x 64 pixels.".format(filename))
         DelayedExit()
     pixels = list(img.getdata())
     bytes = bytearray(int((height // 8) * width))
     i = 0
     b = 0
-    for y in range (0,height,8):
-        for x in range (0,width):
-            for p in range (0,8):
-                b = b >> 1  
+    for y in range(0, height, 8):
+        for x in range(0, width):
+            for p in range(0, 8):
+                b = b >> 1
                 if pixels[(y + p) * width + x] > 0:
                     b |= 0x80
             bytes[i] = b
             i += 1
     return bytes
-    
+
+
 def LoadHexFileData(filename):
     if not os.path.isabs(filename):
-      filename = path + filename
-    if not os.path.isfile(filename) :
+        filename = path + filename
+    if not os.path.isfile(filename):
         return bytearray()
-    f = open(filename,"r")
+    f = open(filename, "r")
     records = f.readlines()
     f.close()
     bytes = bytearray(b'\xFF' * 32768)
     flash_end = 0
-    for rcd in records :
-        if rcd == ":00000001FF" : break
-        if rcd[0] == ":" :
-            rcd_len  = int(rcd[1:3],16)
-            rcd_typ  = int(rcd[7:9],16)
-            rcd_addr = int(rcd[3:7],16)
-            rcd_sum  = int(rcd[9+rcd_len*2:11+rcd_len*2],16)
-            if (rcd_typ == 0) and (rcd_len > 0) :
+    for rcd in records:
+        if rcd == ":00000001FF": break
+        if rcd[0] == ":":
+            rcd_len = int(rcd[1:3], 16)
+            rcd_typ = int(rcd[7:9], 16)
+            rcd_addr = int(rcd[3:7], 16)
+            rcd_sum = int(rcd[9 + rcd_len * 2:11 + rcd_len * 2], 16)
+            if (rcd_typ == 0) and (rcd_len > 0):
                 flash_addr = rcd_addr
                 checksum = rcd_sum
-                for i in range(1,9+rcd_len*2, 2) :
-                    byte = int(rcd[i:i+2],16)
+                for i in range(1, 9 + rcd_len * 2, 2):
+                    byte = int(rcd[i:i + 2], 16)
                     checksum = (checksum + byte) & 0xFF
                     if i >= 9:
                         bytes[flash_addr] = byte
                         flash_addr += 1
                         if flash_addr > flash_end:
                             flash_end = flash_addr
-                if checksum != 0 :
+                if checksum != 0:
                     print("Error: Hex file '{}' contains errors.".format(filename))
                     DelayedExit()
     flash_end = int((flash_end + 255) / 256) * 256
     return bytes[0:flash_end]
 
+
 def LoadDataFile(filename):
     if not os.path.isabs(filename):
-      filename = path + filename
-    if not os.path.isfile(filename) :
+        filename = path + filename
+    if not os.path.isfile(filename):
         return bytearray()
 
-    with open(filename,"rb") as file:
+    with open(filename, "rb") as file:
         bytes = bytearray(file.read())
         pagealign = bytearray(b'\xFF' * (256 - len(bytes) % 256))
         return bytes + pagealign
-        
+
+
 ################################################################################
 
 
-if len(sys.argv) != 2 :
+if len(sys.argv) != 2:
     print("\nUsage: {} flashcart-index.csv\n".format(os.path.basename(sys.argv[0])))
     DelayedExit()
-    
-previouspage = 0xFFFF    
+
+previouspage = 0xFFFF
 currentpage = 0
 nextpage = 0
 csvfile = os.path.abspath(sys.argv[1])
-path = os.path.dirname(csvfile)+os.sep
-if not os.path.isfile(csvfile) :
+path = os.path.dirname(csvfile) + os.sep
+if not os.path.isfile(csvfile):
     print("Error: CSV-file '{}' not found.".format(csvfile))
     DelayedExit()
 TitleScreens = 0
 Sketches = 0
-filename = csvfile.lower().replace("-index","").replace(".csv","-image.bin")
-with open(filename,"wb") as binfile:
-    with open(csvfile,"r") as file:
-        data = csv.reader(file, quotechar='"', delimiter = ";")
-        next(data,None)
+filename = csvfile.lower().replace("-index", "").replace(".csv", "-image.bin")
+with open(filename, "wb") as binfile:
+    with open(csvfile, "r") as file:
+        data = csv.reader(file, quotechar='"', delimiter=";")
+        next(data, None)
         print("Building: {}\n".format(filename))
         print("List Title                     Curr. Prev. Next  ProgSize DataSize SaveSize")
         print("---- ------------------------- ----- ----- ----- -------- -------- --------")
         for row in data:
-            while len(row) < 7: row.append('') #add missing cells
+            while len(row) < 7: row.append('')  # add missing cells
             header = DefaultHeader()
             title = LoadTitleScreenData(row[ID_TITLESCREEN])
             program = LoadHexFileData(row[ID_HEXFILE])
@@ -133,16 +139,16 @@ with open(filename,"wb") as binfile:
             datasize = len(datafile)
             slotsize = ((programsize + datasize) >> 8) + 5
             programpage = currentpage + 5
-            datapage    = programpage + (programsize >> 8)
+            datapage = programpage + (programsize >> 8)
             nextpage += slotsize
-            header[7] = int(row[ID_LIST]) #list number
+            header[7] = int(row[ID_LIST])  # list number
             header[8] = previouspage >> 8
             header[9] = previouspage & 0xFF
             header[10] = nextpage >> 8
             header[11] = nextpage & 0xFF
             header[12] = slotsize >> 8
             header[13] = slotsize & 0xFF
-            header[14] = programsize >> 7 #program size in 128 byte pages
+            header[14] = programsize >> 7  # program size in 128 byte pages
             if programsize > 0:
                 header[15] = programpage >> 8
                 header[16] = programpage & 0xFF
@@ -159,9 +165,12 @@ with open(filename,"wb") as binfile:
             binfile.write(program)
             binfile.write(datafile)
             if programsize == 0:
-              print("{:4} {:25} {:5} {:5} {:5}".format(row[ID_LIST],row[ID_TITLE],currentpage,previouspage,nextpage))
+                print("{:4} {:25} {:5} {:5} {:5}".format(row[ID_LIST], row[ID_TITLE], currentpage, previouspage,
+                                                         nextpage))
             else:
-              print("{:4}  {:24} {:5} {:5} {:5} {:8} {:8} {:8}".format(row[ID_LIST],row[ID_TITLE][:24],currentpage,previouspage,nextpage,programsize,datasize,0))
+                print("{:4}  {:24} {:5} {:5} {:5} {:8} {:8} {:8}".format(row[ID_LIST], row[ID_TITLE][:24], currentpage,
+                                                                         previouspage, nextpage, programsize, datasize,
+                                                                         0))
             previouspage = currentpage
             currentpage = nextpage
             if programsize > 0:
@@ -170,6 +179,7 @@ with open(filename,"wb") as binfile:
                 TitleScreens += 1
         print("---- ------------------------- ----- ----- ----- -------- -------- --------")
         print("                                Page  Page  Page    Bytes    Bytes    Bytes")
-                
-print("\nImage build complete with {} Title screens, {} Sketches, {} Kbyte used.".format(TitleScreens,Sketches,(nextpage+3) / 4))
+
+print("\nImage build complete with {} Title screens, {} Sketches, {} Kbyte used.".format(TitleScreens, Sketches,
+                                                                                         (nextpage + 3) / 4))
 DelayedExit
--- sketch-erase.py
+++ sketch-erase.py
@@ -1,29 +1,29 @@
 print("\nArduboy python sketch eraser v1.0 by Mr.Blinky May 2018")
 
-#requires pyserial to be installed. Use "pip install pyserial" on commandline
+# requires pyserial to be installed. Use "pip install pyserial" on commandline
 
-#rename this script filename to 'uploader-1309.py' to patch uploads on the fly
-#for use with SSD1309 displays
+# rename this script filename to 'uploader-1309.py' to patch uploads on the fly
+# for use with SSD1309 displays
 
 import sys
 import time
-import os
-from serial.tools.list_ports  import comports
+
 from serial import Serial
+from serial.tools.list_ports import comports
 
 compatibledevices = [
- #Arduboy Leonardo
- "VID:PID=2341:0036", "VID:PID=2341:8036",
- "VID:PID=2A03:0036", "VID:PID=2A03:8036",
- #Arduboy Micro
- "VID:PID=2341:0037", "VID:PID=2341:8037",
- "VID:PID=2A03:0037", "VID:PID=2A03:8037",
- #Genuino Micro
- "VID:PID=2341:0237", "VID:PID=2341:8237",
- #Sparkfun Pro Micro 5V
- "VID:PID=1B4F:9205", "VID:PID=1B4F:9206",
- #Adafruit ItsyBitsy 5V
- "VID:PID=239A:000E", "VID:PID=239A:800E",
+    # Arduboy Leonardo
+    "VID:PID=2341:0036", "VID:PID=2341:8036",
+    "VID:PID=2A03:0036", "VID:PID=2A03:8036",
+    # Arduboy Micro
+    "VID:PID=2341:0037", "VID:PID=2341:8037",
+    "VID:PID=2A03:0037", "VID:PID=2A03:8037",
+    # Genuino Micro
+    "VID:PID=2341:0237", "VID:PID=2341:8237",
+    # Sparkfun Pro Micro 5V
+    "VID:PID=1B4F:9205", "VID:PID=1B4F:9206",
+    # Adafruit ItsyBitsy 5V
+    "VID:PID=239A:000E", "VID:PID=239A:800E",
 ]
 
 bootloader_active = False
@@ -31,67 +31,72 @@ caterina_overwrite = False
 
 flash_addr = 0
 flash_data = bytearray(chr(0xFF) * 32768)
-flash_page       = 1
+flash_page = 1
 flash_page_count = 0
-flash_page_used  = [False] * 256
+flash_page_used = [False] * 256
+
 
 def delayedExit():
-  time.sleep(3)
-  #raw_input()  
-  sys.exit()
+    time.sleep(3)
+    # raw_input()
+    sys.exit()
+
 
 def getComPort(verbose):
-  global bootloader_active
-  devicelist = list(comports())
-  for device in devicelist:
-    for vidpid in compatibledevices:
-      if  vidpid in device[2]:
-        port=device[0]
-        bootloader_active = (compatibledevices.index(vidpid) & 1) == 0
-        if verbose : print("Found {} at port {}".format(device[1],port))
-        return port
-  if verbose : print("Arduboy not found.")
+    global bootloader_active
+    devicelist = list(comports())
+    for device in devicelist:
+        for vidpid in compatibledevices:
+            if vidpid in device[2]:
+                port = device[0]
+                bootloader_active = (compatibledevices.index(vidpid) & 1) == 0
+                if verbose: print("Found {} at port {}".format(device[1], port))
+                return port
+    if verbose: print("Arduboy not found.")
+
 
 def bootloaderStart():
-  global bootloader
-  ## find and connect to Arduboy in bootloader mode ##
-  port = getComPort(True)
-  if port is None : delayedExit()
-  if not bootloader_active:
-    print("Selecting bootloader mode...")
-    bootloader = Serial(port,1200)
-    bootloader.close()
-    #wait for disconnect and reconnect in bootloader mode
-    while getComPort(False) == port :
-      time.sleep(0.1)
-      if bootloader_active: break        
-    while getComPort(False) is None : time.sleep(0.1)
+    global bootloader
+    ## find and connect to Arduboy in bootloader mode ##
     port = getComPort(True)
-  
-  time.sleep(0.1)	
-  bootloader = Serial(port,57600)
-  
+    if port is None: delayedExit()
+    if not bootloader_active:
+        print("Selecting bootloader mode...")
+        bootloader = Serial(port, 1200)
+        bootloader.close()
+        # wait for disconnect and reconnect in bootloader mode
+        while getComPort(False) == port:
+            time.sleep(0.1)
+            if bootloader_active: break
+        while getComPort(False) is None: time.sleep(0.1)
+        port = getComPort(True)
+
+    time.sleep(0.1)
+    bootloader = Serial(port, 57600)
+
+
 def bootloaderExit():
-  global bootloader
-  bootloader.write("E")
-  bootloader.read(1)
-  
+    global bootloader
+    bootloader.write("E")
+    bootloader.read(1)
+
+
 ################################################################################
 
-bootloaderStart()      
-      
+bootloaderStart()
+
 ## Erase ##
 print("\nErasing sketch startup page")
-bootloader.write("A\x00\x00") #select page 0
+bootloader.write("A\x00\x00")  # select page 0
 bootloader.read(1)
-bootloader.write("B\x00\x00F") #writing 0 length block will erase page only
+bootloader.write("B\x00\x00F")  # writing 0 length block will erase page only
 bootloader.read(1)
-bootloader.write("A\x00\x00") #select page 0
+bootloader.write("A\x00\x00")  # select page 0
 bootloader.read(1)
-bootloader.write("g\x00\x80F")# read 128 byte page
-if bytearray(bootloader.read(128)) == bytearray("\xff"* 128):
+bootloader.write("g\x00\x80F")  # read 128 byte page
+if bytearray(bootloader.read(128)) == bytearray("\xff" * 128):
     print("\nErase successful")
-else :
+else:
     print("\nErase failed")
-bootloaderExit()      
+bootloaderExit()
 delayedExit()
